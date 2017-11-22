6c566bf49f600108b6ae06a5545a486e4b99971e
zhaochunquan <zhaochunquan@xianrongwang.com>
2017-11-22 11:31:10 +0800

    aaaaa

--- src/testbranch/aa.java
+++ src/testbranch/aa.java
@@ -2,6 +2,7 @@ package testbranch;
 
 public class aa {
 	public static void main(String[] args) {
-		System.out.println(11111111);
+		System.out.println(11111222);
+		System.out.println(222222222);
 	}
 }
